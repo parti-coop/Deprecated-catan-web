1a3aadea25ea1d01fbc4c997d30c08569845278e
dali <dali@ufofactory.org>
2016-01-19 16:27:48 +0900

    Add available locales

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module CatanWeb
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:en, :ko]
     config.i18n.default_locale = :ko
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
